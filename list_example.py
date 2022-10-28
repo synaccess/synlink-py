e786199a7bc7ae0bea075af5d281769f71d343e1
Kevin <khan1782@gmail.com>
2022-10-28 16:40:02 -0700

    add options for password or token

--- list_example.py
+++ list_example.py
@@ -4,30 +4,41 @@
 
 from synlink_py import SynLinkPy
 
-compactPdu1 = SynLinkPy("http://192.168.1.179", "9wxBwnck6JhEH0b1DyI")
-switchedPdu1 = SynLinkPy("http://192.168.1.103", "fJEbchBpEdxr5epxvgb")
+# compactPdu1 = SynLinkPy("http://192.168.1.179", "9wxBwnck6JhEH0b1DyI")
+compactPdu1 = SynLinkPy("http://192.168.1.179", { "token": "9wxBwnck6JhEH0b1DyI" })
 
-compact_outlets = compactPdu1.outlets.list()
-switched_outlets = switchedPdu1.outlets.list()
+# compactPdu1 = SynLinkPy("http://192.168.1.179", { "token": "9wxBwnck6JhEH0b1DyI" })
+switchedPdu1 = SynLinkPy("http://192.168.1.103", { "username": "admin", "password": "admin" })
 
-print("--------")
-for outlet in switched_outlets:
-    print(outlet['id'], outlet['outletName'], outlet['state'])
-print("--------")
+# compact_outlets = compactPdu1.outlets.list()
+# switched_outlets = switchedPdu1.outlets.list()
+print(switchedPdu1.outlets.list())
+print(compactPdu1.inlets.list())
+# print("--------")
+# for outlet in switched_outlets:
+#     print(outlet['id'], outlet['outletName'], outlet['state'])
+# print("--------")
 
-print("state of outlet1:", switched_outlets[0]['state'], switched_outlets[0]['state'] == 'ON')
+# print("state of outlet1:", switched_outlets[0]['state'], switched_outlets[0]['state'] == 'ON')
 
-
-if (switched_outlets[0]['state'] == 'ON'):
-    switchedPdu1.outlets.set_state(0, 'OFF')
-    print("Switched PDU outlet 1 is now OFF") 
-else:
-    switchedPdu1.outlets.set_state(0, 'ON')
-    print("Switched PDU outlet 1 is now ON")
+# print("state of outlet2:", switched_outlets[1]['state'], switched_outlets[1]['state'] == 'ON')
 
 
+# if (switched_outlets[0]['state'] == 'ON'):
+#     switchedPdu1.outlets.set_state(1, 'OFF')
+#     print("Switched PDU outlet 1 is now OFF") 
+# else:
+#     switchedPdu1.outlets.set_state(1, 'ON')
+#     print("Switched PDU outlet 1 is now ON")
+
+
+# if (switched_outlets[1]['state'] == 'ON'):
+#     switchedPdu1.outlets.set_state(2, 'OFF')
+#     print("Switched PDU outlet 2 is now OFF") 
+# else:
+#     switchedPdu1.outlets.set_state(2, 'ON')
+#     print("Switched PDU outlet 2 is now ON")
 
 
 
-
-
+# switchedPdu1.outlets.set_state(1, "OFF")
--- synlink_py/__init__.py
+++ synlink_py/__init__.py
@@ -7,48 +7,26 @@ __version__ = "0.1.0"
 DEFAULT_TIMEOUT = 10
 DEFAULT_RETRIES = 0
 
-__all__ = ['Error', 'Response', 'BaseAPI', 'Outlets', 'SynLinkPy']
+__all__ = ['Error', 'Response', 'BaseAPI', 'Outlets', 'Inlets', 'SynLinkPy']
 
 class Error(Exception):
     pass
 
-class Response(object):
-    def __init__(self, body):
-        self.raw = body
-        self.body = json.loads(body)
-        self.successful = self.body['ok']
-        self.error = self.body.get('error')
-
-    def __str__(self):
-        return json.dumps(self.body)
-
-
-
 class BaseAPI(object):
-    def __init__(self, token=None, host=None, timeout=DEFAULT_TIMEOUT, retries=DEFAULT_RETRIES):
+    def __init__(self, token=None, cookie=None, host=None, timeout=DEFAULT_TIMEOUT, retries=DEFAULT_RETRIES):
         self.token = token
         self.host = host
         self.timeout = timeout
         self.retries = retries
-
-    # def _request(self, request_method, resource, resource_id, **kwargs):
-    #     if self.token:
-    #         kwargs.setdefault('headers', {})['Authorization'] = 'Bearer ' + self.token
-    #     if resource_id:
-    #         url = self.host + "/api/" + resource + '/' + resource_id
-    #     else:
-    #         url = self.host + "/api/" + resource
-        
-    #     response = request_method(url, timeout=self.timeout, **kwargs)
-
-    #     if not response.status_code == 200:
-    #         raise Error(response.reason)
-
-    #     return response.json()
+        self.cookie = cookie
+        # self.username = username
+        # self.password = password
 
     def get(self, resource, **kwargs):
         if self.token:
             kwargs.setdefault('headers', {})['Authorization'] = 'Bearer ' + self.token
+        if self.cookie:
+            kwargs.setdefault('headers', {})['Cookie'] = 'SPID=' + self.cookie
 
         url = self.host + "/api/" + resource
         response = requests.get(url, timeout=self.timeout, **kwargs)
@@ -58,21 +36,14 @@ class BaseAPI(object):
 
         return response.json()
 
-    # def post(self, resource, **kwargs):
-    #     return self._request(
-    #       # self._session_post if self.session else requests.post,
-    #       requests.post,
-    #       resource,
-    #       **kwargs
-    #     )
-
     def put(self, resource, resource_id, **kwargs):
         if self.token:
             kwargs.setdefault('headers', {})['Authorization'] = 'Bearer ' + self.token
 
         url = self.host + "/api/" + resource + '/' + resource_id
         response = requests.put(url, **kwargs)
-
+        print(url)
+        print(vars(response))
         if not response.status_code == 200:
             raise Error(response.reason)
 
@@ -87,8 +58,9 @@ class Outlets(BaseAPI):
         :param state: The state to set the outlet to. Can be "ON" or "OFF", or "REBOOT".
         :return: The response from the API.
         """
-        # TODO check if state is correct
-        
+        if state != "REBOOT" and state != "ON" and state != "OFF":
+            raise Error("State must be 'ON', 'OFF', or 'REBOOT'")
+
         data_json_string = json.dumps({"state": state})
         return self.put("outlets", str(outlet), data=data_json_string)
     
@@ -100,14 +72,38 @@ class Outlets(BaseAPI):
 
         return self.get("outlets")
 
+class Inlets(BaseAPI):
+    def list(self):
+        """List all inlets.
+
+        :return: The response from the API.
+        """
+
+        return self.get("inlets")
+
 
 class SynLinkPy(object):
-  def __init__(self, host, token, timeout=DEFAULT_TIMEOUT):
-
+  def __init__(self, host, credentials, timeout=DEFAULT_TIMEOUT):
+      credentials.get("username")
+      credentials.get("token")
       api_args = {
           'host': host,
-          'token': token,
           'timeout': timeout,
       }
+      # if no token, then login and with user name and password and set cookie
+      if (credentials.get("token") != None):
+          print("Token used")
+          api_args["token"] = credentials["token"]
+
+      elif (credentials.get("token") == None and credentials.get("username") != None and credentials.get("password") != None):
+          print("No token, username and password though")
+          res = requests.post(host + "/login", data=json.dumps({ "username": credentials["username"], "password": credentials["password"] }))
+          if res.status_code != 200:
+              raise Error("Login failed")
+          api_args["cookie"] = res.cookies["SPID"]
+
+      else:
+          raise Error("No token or username and password provided.")
+
       self.outlets = Outlets(**api_args)
-      # self.inlets = Inlets(**api_args)
+      self.inlets = Inlets(**api_args)
